b095cfdff50d8449ac0f3689645b53c71ba14178
shrest21 <shrest21@gmail.com>
2025-12-16 22:12:27 +0530

    Added basic flights page and tested if angular localhost runs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { BoardUserComponent } from './board-user/board-user.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { FlightsComponent } from './flights/flights.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'user', component: BoardUserComponent },
   { path: 'mod', component: BoardModeratorComponent },
   { path: 'admin', component: BoardAdminComponent },
+  { path: 'flights', component: FlightsComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
---
+++ src/app/flights/flights.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flights',
+  templateUrl: './flights.component.html',
+  styleUrls: ['./flights.component.css']
+})
+export class FlightsComponent {
+}
