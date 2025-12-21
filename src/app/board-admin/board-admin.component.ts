98808f6fc600d93aadce9e390feca3637ba3e546
shrest21 <shrest21@gmail.com>
2025-12-21 22:31:38 +0530

    Added add flights feature to admin role only and works succesfully

--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from '../_services/storage.service';
+import { HttpClient } from '@angular/common/http';
+import  {AddFlightService} from '../_services/addflight.service';
 
 @Component({
   selector: 'app-board-admin',
@@ -8,29 +10,61 @@ import { StorageService } from '../_services/storage.service';
 })
 export class BoardAdminComponent implements OnInit {
 
-  constructor(
-    private storageService: StorageService,
-    private router: Router
-  ) {}
+  flight = {
+    airline: '',
+    fromPlace: '',
+    toPlace: '',
+    flightName: '',
+    departureTime: '',
+    arrivalTime: '',
+    flightDate: '',
+    totalSeats: 0,
+    price: 0
+  };
+
+  constructor( private storageService: StorageService, private router: Router, private addFlightService: AddFlightService) {}
 
   ngOnInit(): void {
     const user = this.storageService.getUser();
-
-    // Not logged in
     if (!user) {
       alert('Please login as admin');
       this.router.navigate(['/login']);
       return;
     }
-
-    // Logged in but not admin
     if (!user.roles || !user.roles.includes('ROLE_ADMIN')) {
       alert('Login as admin to access this page');
       this.router.navigate(['/login']);
       return;
     }
 
-    // ✅ Admin → allow page
     console.log('Admin access granted');
+  }
+  addFlight(): void {
+    console.log('Add Flight Payload:', this.flight);
+
+    this.addFlightService.addFlight(this.flight).subscribe({
+      next: (response) => {
+        alert('Flight added successfully');
+        console.log(response);
+        this.resetForm();
+      },
+      error: (error) => {
+        console.error(error);
+        console.log('Failed to add flight');
+      }
+    });
+  }
+  private resetForm(): void {
+    this.flight = {
+      airline: '',
+      fromPlace: '',
+      toPlace: '',
+      flightName: '',
+      departureTime: '',
+      arrivalTime: '',
+      flightDate: '',
+      totalSeats: 0,
+      price: 0
+    };
   }
 }
