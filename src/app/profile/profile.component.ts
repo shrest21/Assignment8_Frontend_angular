19cb404c8c7aaf0676215db1ea0bfbedddc90275
shrest21 <shrest21@gmail.com>
2025-12-22 01:55:34 +0530

    Change password updating

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,10 +8,31 @@ import { StorageService } from '../_services/storage.service';
 })
 export class ProfileComponent implements OnInit {
   currentUser: any;
+  days=0;
+  showPasswordPopup = false;
 
   constructor(private storageService: StorageService) { }
 
   ngOnInit(): void {
     this.currentUser = this.storageService.getUser();
+    if(this.currentUser==null){
+      alert("Please login");
+      window.location.href='/login';
+    }
   }
+  checkPassword(lastChange: string): void{
+    const lastDate = new Date(lastChange);
+    const today = new Date();
+    const diffTime = today.getTime() - lastDate.getTime();
+    this.days=Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    if(this.days>10)
+    {
+      this.showPasswordPopup = true;
+    }
+  }
+
+  goToChangePassword(): void {
+    window.location.href = '/change';
+  }
+
 }
